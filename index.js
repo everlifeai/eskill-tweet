ad18a7efc56323814fe955f118e039df6167e143
Rajeshkannan R <rrajeshkannanbca@gmail.com>
2019-10-01 14:01:34 +0530

    Add Additional tweet actions

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const cote = require('cote')
 const util = require('./util')
 const u = require('elife-utils')
+const comment = require('./tweet/comment')
+const retweet = require('./tweet/retweet')
+const like = require('./tweet/like')
+const follow = require('./tweet/follow')
+const unfollow = require('./tweet/unfollow')
+
 
 /* microservice key (identity of the microservice) */
 let msKey = 'eskill_tweet'
@@ -92,41 +98,227 @@ function startMicroservice() {
      */
     svc.on('msg', (req, cb) => {
         if(!req.msg) return cb()
-        if(!req.msg.startsWith('/tweet')) return cb()
-        cb(null, true)
-        if(!auth || !auth.username || !auth.password) sendReply(errmsg.LEVELERR, req)
-        else {
-            let tweet_msg = req.msg.substr('/tweet'.length).trim() 
-            if(tweet_msg.length > 280) {
-                sendReply('Tweet message character limit exists. Try with a different message.',req)
+        if(req.msg.startsWith('/tweet')){
+            if(!auth || !auth.username || !auth.password){
+                sendReply(errmsg.LEVELERR, req)
+            } 
+            else {
+                let tweet_msg = req.msg.substr('/tweet'.length).trim() 
+                if(tweet_msg.length > 280) {
+                    sendReply('Tweet message character limit exists. Try with a different message.',req)
+                }else {
+                    util.tweet(auth.username, auth.password, tweet_msg)
+                        .then((result) => {
+                            if(result && result.success)
+                                sendReply("Tweeted successfully.", req)
+                            else 
+                                sendReply('Tweet failed.', req)
+                        })
+                        .catch((err) => {
+                            u.showErr(err)
+                            sendReply('Tweet failed..', req)
+                        })
+                }
+            }
+        } else if(req.msg.startsWith('/retweet')){
+            
+            cb(null, true)
+            if(!auth || !auth.username || !auth.password){
+                sendReply(errmsg.LEVELERR, req)
             }else {
-                util.tweet(auth.username, auth.password, tweet_msg)
-                    .then((result) => {
-                        if(result && result.success)
-                            sendReply("Tweeted successfully.", req)
-                        else 
-                            sendReply('Tweet failed.', req)
+                let tweet_statusId = req.msg.substr('/retweet '.length).trim()
+                retweet.reTweet(auth.username,auth.password,tweet_statusId)
+                .then((result)=>{
+                    if(result && result.sucess)
+                        sendReply("Retweeted successfully.",req)
+                    else
+                        sendReply('Retweet failed.',req)
+                })
+                .catch((err) =>{
+                    u.showErr(err)
+                    sendReply('Retweet failed..',req)
+                
+                }) 
+            }
+        }else if(req.msg.startsWith('/like')){
+            cb(null, true)
+            if(!auth || !auth.username || !auth.password){
+                sendReply(errmsg.LEVELERR, req)
+            }else {
+                let like_statusId = req.msg.substr('/like '.length).trim()
+                like.likeButton(auth.username,auth.password,like_statusId)
+                    .then((result)=>{
+                        if(result && result.sucess)
+                            sendReply("Liked successfully.",req)
+                        else
+                            sendReply('Like failed.',req)
+                    })
+                    .catch((err) =>{
+                        u.showErr(err)
+                        sendReply('Like failed..',req)
+                    })
+                }
+        } else{
+            cb()
+        }
+        
+        
+            
+            if(msg.startsWith('/comment')){
+                cb(null, true)
+                let  x= req.msg.substr('/comment'.length).trim()
+                let status_Id=x.substr(0,x.indexOf(' '))
+                let tweet_comment=x.substr(x.indexOf(' ')+1);
+
+                
+                comment.commentTweet(auth.username,auth.password,status_Id,tweet_comment)
+                    .then((result)=>{
+                        if(result && result.sucess)
+                            sendReply("Commented tweet successfully.",req)
+                        else
+                            sendReply('Comment failed.',req)
+                    })
+                    .catch((err) =>{
+                        u.showErr(err)
+                        sendReply('failed..',req)
+                    })
+                }
+            if(msg.startsWith('/like')){
+                cb(null, true)
+                let like_statusId = req.msg.substr('/like '.length).trim()
+                like.likeButton(auth.username,auth.password,like_statusId)
+                    .then((result)=>{
+                        if(result && result.sucess)
+                            sendReply("Liked successfully.",req)
+                        else
+                            sendReply('Like failed.',req)
+                    })
+                    .catch((err) =>{
+                        u.showErr(err)
+                        sendReply('Like failed..',req)
+                    })
+
+            }
+            if(msg.startsWith('/follow')){
+                cb(null, true)
+                let follow_Id = req.msg.substr('/follow'.length).trim()
+                follow.follow(auth.username,auth.password,follow_Id)
+                    .then((result)=>{
+                        if(result && result.sucess)
+                            sendReply("Followed successfully.",req)
+                        else
+                            sendReply('Follow failed.',req)
+                    })
+                    .catch((err) =>{
+                        u.showErr(err)
+                        sendReply('Follow failed..',req)
+                    })
+
+            }
+            if(msg.startsWith('/unfollow')){
+                cb(null, true)
+                let unfollow_Id = req.msg.substr('/unfollow '.length).trim()
+                unfollow.follow(auth.username,auth.password,unfollow_Id)
+                    .then((result)=>{
+                        if(result && result.sucess)
+                            sendReply("Unfollowed successfully.",req)
+                        else
+                            sendReply('Unfollow failed.',req)
+                    })
+                    .catch((err) =>{
+                        u.showErr(err)
+                        sendReply('Unfollow failed..',req)
+                    })
+
+                }
+            else{
+                cb()
+            }
+            
+        
+        
+        })
+    svc.on('task', (req, cb) => { 
+        
+        try{
+            
+            let data = JSON.parse(util.getData(req.task)); 
+            console.log(data)
+            if(!auth || !auth.username || !auth.password) cb('Twitter credentials missing.')
+            else if(data.type == 'tweet'){
+                util.tweet(auth.username, auth.password, data.message)
+                    .then((result)=>{
+                        cb(null, req.task, result)
                     })
                     .catch((err) => {
                         u.showErr(err)
-                        sendReply('Tweet failed..', req)
+                        cb('Something went wrong.')
                     })
+                    
             }
-        }
-        
-    })
-
-    svc.on('task', (req, cb) => { 
-        if(!auth || !auth.username || !auth.password) cb('Twitter credentials missing.')
-        else{
-            util.tweet(auth.username, auth.password, util.getTweetMsg(req.task))
-                .then((result)=>{
-                    cb(null, req.task, result)
-                })
-                .catch((err) => {
-                    u.showErr(err)
-                    cb('Something went wrong.')
-                })
+            
+            else if(data.type == 'retweet'){
+                console.log(data.uri)
+                retweet.reTweet(auth.username, auth.password, data.uri)
+                    .then((result)=>{
+                        console.log(result)
+                        cb(null, req.task, result)
+                    })
+                    .catch((err) => {
+                        u.showErr(err)
+                        cb('Something went wrong.')
+                    })
+                    
+            }
+            else if(data.type == 'comment'){
+                comment.commentTweet(auth.username, auth.password,data.uri,data.comment)
+                    .then((result)=>{
+                        cb(null, req.task, result)
+                    })
+                    .catch((err) => {
+                        u.showErr(err)
+                        cb('Something went wrong.')
+                    })
+                    
+            }
+            
+            else if(data.type == 'like'){
+                like.likeButton(auth.username, auth.password, data.uri)
+                    .then((result)=>{
+                        cb(null, req.task, result)
+                    })
+                    .catch((err) => {
+                        u.showErr(err)
+                        cb('Something went wrong.')
+                    })
+                    
+            }
+            
+            else if(data.type == 'follow'){
+                follow.follow(auth.username, auth.password, data.uri)
+                    .then((result)=>{
+                        cb(null, req.task, result)
+                    })
+                    .catch((err) => {
+                        u.showErr(err)
+                        cb('Something went wrong.')
+                    })
+                    
+            }
+            else if(data.type == 'unfollow'){
+                unfollow.unfollow(auth.username, auth.password, data.uri)
+                    .then((result)=>{
+                        cb(null, req.task, result)
+                    })
+                    .catch((err) => {
+                        u.showErr(err)
+                        cb('Something went wrong.')
+                    })
+                    
+            }
+        }catch(e){
+            console.log(e)
+            cb('Something went wrong')
         }
     })
 }
@@ -137,7 +329,13 @@ function registerWithCommMgr() {
         mskey: msKey,
         mstype: 'msg',
         mshelp: [ 
-            { cmd: '/tweet', txt: 'Send a Tweet' }  ],
+            {cmd: '/tweet', txt: 'Send a Tweet' },
+            {cmd: '/retweet', txt: 'For retweet'},
+            {cmd: '/comment', txt: 'For commenting a tweet'},
+            {cmd: '/like', txt: 'For like a tweet'},
+            {cmd: '/follow', txt: 'For following '},
+            {cmd: '/unfollow', txt: 'For unfollowing '},
+        ],
     }, (err) => {
         if(err) u.showErr(err)
     })
